2a639c407a5d1adf91c5afe9d933529a3bab70c2
rattada <rattada@evoketechnologies.com>
2014-08-21 17:57:54 +0530

    changes done...

--- 123Movers/Scripts/getBudget.js
+++ 123Movers/Scripts/getBudget.js
@@ -1,18 +1,6 @@
 ﻿$(function () {
-    var activeCount = $("#ActiveInfo > tbody > tr").length;
-    var expCount = $("#ExpiredInfo > tbody > tr").length;
-    var text = $("#ActiveInfo > tbody > tr > td").text().trim();
-    var val = 'No record(s) found';
-    if (text != val) {
-        if (activeCount == 1) {
-            $('#ExpiredInfo tr:last').remove();
-        }
-        else if (activeCount == 2) {
-            $('#ExpiredInfo tr:last').remove();
-            $('#ExpiredInfo tr:last').remove();
-        }
-        $('#ExpiredInfo').dataTable({ "sPaginationType": "full_numbers" });
-    }
+ 
+    $('#ExpiredInfo').dataTable({ "sPaginationType": "full_numbers" });
 
     $('.renewBudget').click(function () {
         var serviceId = $(this).attr('data-id');
