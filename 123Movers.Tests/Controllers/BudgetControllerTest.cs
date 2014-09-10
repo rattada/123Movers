961a8ef09d0b37a015759bf5bde959a4cd1df28d
rattada <rattada@evoketechnologies.com>
2014-09-10 16:00:07 +0530

    changes done

--- 123Movers.Tests/Controllers/BudgetControllerTest.cs
+++ 123Movers.Tests/Controllers/BudgetControllerTest.cs
@@ -30,29 +30,5 @@ namespace _123Movers.Tests.Controllers
             };
             return budget;
         }
-
-        //[TestMethod]
-        //public void GetBudgetsForCompany()
-        //{
-        //    IEnumerable<BudgetModel> budgetList = new List<BudgetModel>();
-        //    budgetList = BusinessLayer.GetBudget(companyId);
-
-        //    Assert.IsNotNull(budgetList, "The request did not return any results.");
-        //    Assert.IsTrue(budgetList.Count() > 0, "The returned collection is empty.");
-        //}
-
-        //[TestMethod]
-        //public void AddBudgetForCompany()
-        //{
-        //    var newBudget = CreateBudget(1000, "123456", true, false, "NEW INSERTION");
-
-        //    BusinessLayer.SaveBudget(newBudget);
-
-        //    BudgetModel budget = new BudgetModel();
-        //    budget = BusinessLayer.GetBudget(companyId).Where(b => b.CompanyId == companyId && b.ServiceId == newBudget.ServiceId && b.AgreementNumber == newBudget.AgreementNumber).FirstOrDefault();
-
-        //    Assert.IsNotNull(budget, "The request did not return any results.");
-        //    Assert.AreEqual(budget.AgreementNumber, newBudget.AgreementNumber);
-        //}
     }
 }
